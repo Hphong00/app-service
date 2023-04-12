2a33b8044d71c42305a5516e5a9d064e75ac0fc4
hoangphong <hoangphong@macbook-cua-phong.local>
2023-04-12 16:54:44 +0700

    upfile

---
+++ productservice/src/main/java/com/app/productservice/client/OAuth2InterceptedFeignConfiguration.java
@@ -0,0 +1,13 @@
+package com.app.productservice.client;
+
+import com.app.productservice.security.oauth2.AuthorizationHeaderUtil;
+import feign.RequestInterceptor;
+import org.springframework.context.annotation.Bean;
+
+public class OAuth2InterceptedFeignConfiguration {
+
+    @Bean(name = "oauth2RequestInterceptor")
+    public RequestInterceptor getOAuth2RequestInterceptor(AuthorizationHeaderUtil authorizationHeaderUtil) {
+        return new TokenRelayRequestInterceptor(authorizationHeaderUtil);
+    }
+}
