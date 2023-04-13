d0c042175483b8ac2dec12d6ed339e1b8236e85f
hoangphong <hoangphong@macbook-cua-phong.local>
2023-04-13 18:44:26 +0700

    Initial version of orderservice generated by generator-jhipster@7.9.3

---
+++ orderservice/src/main/java/com/app/orderservice/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.app.orderservice.repository;
+
+import com.app.orderservice.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
